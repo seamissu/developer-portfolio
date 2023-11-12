35f5a53bb8d459b5a5ba2afd5e618edffd209966
Jingning <seamissu@gmail.com>
2023-11-13 09:39:24 +1100

    create HTML structure

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,54 @@ import GlobalStyles from '@/components/GlobalStyles';
 export default function Home() {
   return (
     <>
-      <Wrapper>abc</Wrapper>
+      <Wrapper>
+        <header>header</header>
+        <section>experience</section>
+        <main>projects</main>
+        <div>form</div>
+        <footer>footer</footer>
+      </Wrapper>
+
       <GlobalStyles />
     </>
   );
 }
 
 const Wrapper = styled.div`
-  background-color: red;
+  display: grid;
+  min-height: 100%;
+  grid-template-areas:
+    'header'
+    'experience'
+    'projects'
+    'form'
+    'footer';
+  grid-template-columns: 820px 320px 1800px 500px 180px;
+
+  & > header,
+  & > section,
+  & > main,
+  & > div,
+  & > footer {
+    border: 2px solid black;
+  }
+
+  & > header {
+    grid-area: header;
+  }
+  & > section {
+    grid-area: experience;
+  }
+
+  & > main {
+    grid-area: projects;
+  }
+
+  & > div {
+    grid-area: form;
+  }
+
+  & > footer {
+    grid-area: footer;
+  }
 `;
